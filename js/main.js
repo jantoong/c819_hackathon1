ae8ca201c9f6f900e5d11e017cf8ef98ec3fe0ac
Edward W. Lee <elee5696@gmail.com>
2019-09-05 13:39:13 -0700

    zombie vs players, players run away

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(drawBoard);
 
 var tileIDCounter = 0;
 var game = new Game();
+var itemUsed;
 
 function drawBoard() {
   var board = new CreateBoard();
@@ -12,8 +13,6 @@ function drawBoard() {
 }
 
 function createDOM() {
-  game.createNewPlayer();
-  game.createNewPlayer();
   game.createZombies();
   game.renderItems();
 }
